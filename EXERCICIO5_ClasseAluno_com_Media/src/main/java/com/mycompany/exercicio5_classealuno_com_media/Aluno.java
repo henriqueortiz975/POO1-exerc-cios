cc2f94b2dd8c3eb432d4642cbd3c72ab380d7d1a
henriqueortiz975 <henriqueortiz3090@gmail.com>
2026-02-25 19:22:19 -0300

    finalizado

---
+++ EXERCICIO5_ClasseAluno_com_Media/src/main/java/com/mycompany/exercicio5_classealuno_com_media/Aluno.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.exercicio5_classealuno_com_media;
+
+/**
+ *
+ * @author User
+ */
+public class Aluno {
+    
+}
---
+++ EXERCICIO5_ClasseAluno_com_Media/src/main/java/com/mycompany/exercicio5_classealuno_com_media/EXERCICIO5_ClasseAluno_com_Media.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exercicio5_classealuno_com_media;
+
+/**
+ *
+ * @author User
+ */
+public class EXERCICIO5_ClasseAluno_com_Media {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
