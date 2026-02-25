5332128d3636ebfd7f66f5f80f0a47e21e303e7f
henriqueortiz975 <henriqueortiz3090@gmail.com>
2026-02-25 14:11:12 -0300

    Finalizado

---
+++ EXERCICIO1_ClassePessoa_NIvel__Basico/src/main/java/Pessoa.java
@@ -0,0 +1,12 @@
+package com.mycompany.exercicio1_classepessoa_nivel__basico;
+
+public class Pessoa{
+    String nome;
+    int idade;
+
+
+public void apresentar(){
+    System.out.println("Olá, meu nome é " + nome + " e tenho "+ idade +" anos.");
+}
+
+}
---
+++ EXERCICIO1_ClassePessoa_NIvel__Basico/src/main/java/com/mycompany/exercicio1_classepessoa_nivel__basico/EXERCICIO1_ClassePessoa_NIvel__Basico.java
@@ -0,0 +1,12 @@
+package com.mycompany.exercicio1_classepessoa_nivel__basico;
+
+public class EXERCICIO1_ClassePessoa_NIvel__Basico {
+
+    public static void main(String[] args) {
+       Pessoa objPessoa = new Pessoa(); 
+       objPessoa.idade = 17;
+       objPessoa.nome = "Henrique";
+       objPessoa.apresentar();
+        
+    }
+}
