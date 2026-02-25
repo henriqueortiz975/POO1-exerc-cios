5768323a2777894b6dc61b8b5125cec73c3ef711
henriqueortiz975 <henriqueortiz3090@gmail.com>
2026-02-25 20:33:23 -0300

    finalizado

--- EXERCICIO5_ClasseAluno_com_Media/src/main/java/com/mycompany/exercicio5_classealuno_com_media/Aluno.java
+++ EXERCICIO5_ClasseAluno_com_Media/src/main/java/com/mycompany/exercicio5_classealuno_com_media/Aluno.java
@@ -1,13 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.exercicio5_classealuno_com_media;
 
-/**
- *
- * @author User
- */
 public class Aluno {
+    String nome;
+    double nota1;
+    double nota2;
     
+    public void calcularMedia(){
+        System.out.println("nome do aluno: " + nome);
+        System.out.println("a primeira nota do aluno: " + nota1);
+        System.out.println("a segunda nota do aluno: " + nota2);
+        nota1 += nota2;
+        System.out.println("");
+    }
+    public void verificarSituacao(){
+        nota1 /= 2;
+        if(nota1 >= 7){
+            System.out.println("Aprovado");
+        }else if(nota1 >= 5){
+            System.out.println("Recuperação");
+        }else{
+            System.out.println("Reprovado");
+        }
+        
+
+    }
 }
--- EXERCICIO5_ClasseAluno_com_Media/src/main/java/com/mycompany/exercicio5_classealuno_com_media/EXERCICIO5_ClasseAluno_com_Media.java
+++ EXERCICIO5_ClasseAluno_com_Media/src/main/java/com/mycompany/exercicio5_classealuno_com_media/EXERCICIO5_ClasseAluno_com_Media.java
@@ -1,16 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.exercicio5_classealuno_com_media;
 
-/**
- *
- * @author User
- */
 public class EXERCICIO5_ClasseAluno_com_Media {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Aluno objaluno = new Aluno();
+        objaluno.nome = "Henrique Ibarra Ortiz Urbano";
+        objaluno.nota1 = 7;
+        objaluno.nota2 = 8;
+        objaluno.calcularMedia();
+        objaluno.verificarSituacao();
+        
     }
 }
